5f7f4b357b560eea8f705807db33682ca3c047b0
Christian Aguilar <106763131+christianaguilarm@users.noreply.github.com>
2024-05-08 22:38:38 -0400

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var selectsNegocio = document.querySelectorAll("#matrizNegocio select");
+    var calcularNegocio = document.getElementById("calcularNegocio");
+    var resultadoNegocio = document.getElementById("resultadoNegocio");
+
+    calcularNegocio.addEventListener("click", function() {
+        var totalPuntaje = 0;
+        var allSelected = true;
+
+        selectsNegocio.forEach(function(select) {
+            if (select.value === "") {
+                allSelected = false;
+                return;
+            }
+
+            totalPuntaje += parseInt(select.value);
+        });
+
+        if (allSelected) {
+            var complejidad = calcularComplejidad(totalPuntaje);
+            resultadoNegocio.innerText = "La suma de los puntajes seleccionados es: " + totalPuntaje + ". Complejidad: " + complejidad;
+        } else {
+            resultadoNegocio.innerText = "Debe seleccionar una opción en cada select.";
+        }
+    });
+
+    function calcularComplejidad(puntaje) {
+        if (puntaje >= 8 && puntaje <= 12) {
+            return "Baja";
+        } else if (puntaje >= 13 && puntaje <= 20) {
+            return "Media";
+        } else {
+            return "Alta";
+        }
+    }
+
+    var selectsTecnologia = document.querySelectorAll("#matrizTecnologia select");
+    var calcularTecnologia = document.getElementById("calcularTecnologia");
+    var resultadoTecnologia = document.getElementById("resultadoTecnologia");
+
+    calcularTecnologia.addEventListener("click", function() {
+        var totalPuntaje = 0;
+        var allSelected = true;
+
+        selectsTecnologia.forEach(function(select) {
+            if (select.value === "") {
+                allSelected = false;
+                return;
+            }
+
+            totalPuntaje += parseInt(select.value);
+        });
+
+        if (allSelected) {
+            var complejidad = calcularComplejidad(totalPuntaje);
+            resultadoTecnologia.innerText = "La suma de los puntajes seleccionados es: " + totalPuntaje + ". Complejidad: " + complejidad;
+        } else {
+            resultadoTecnologia.innerText = "Debe seleccionar una opción en cada select.";
+        }
+    });
+});
